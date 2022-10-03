e97b3ffaf74524503c1f8cdfc58604e1f5d19276
blackbotalpha <113766233+blackbotalpha@users.noreply.github.com>
2022-10-03 16:40:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ module.exports = {
     bot: "bot", // defult: "This Feature Is Only For Bot",
     wait: "waiting ", // defult: "In process...",
     linkm: "not", // defult: "Where is the link?",    error: "errow is this ", / defult: "Error!!",
-    endLimit: "", // defult: "Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours",
-    ban: "", // defult: "You have been banned by the owner, if you want to be unbanned, chat owner.",
-    nsfw: "", // defult: "The nsfw feature has not been activated, please contact the admin to activate",
-    banChat: "", // defult: "The bot was banned in this group, please contact the owner to unban",
+    endLimit: "full limid", // defult: "Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours",
+    ban: "band now", // defult: "You have been banned by the owner, if you want to be unbanned, chat owner.",
+    nsfw: "not", // defult: "The nsfw feature has not been activated, please contact the admin to activate",
+    banChat: "band this chat", // defult: "The bot was banned in this group, please contact the owner to unban",
   },
   setting: {
     blockchat: [], // Your block chat Jids
@@ -26,14 +26,14 @@ module.exports = {
   auto: {
     chat: {
       group: false, // Chat Bot In Group | u can set true or false
-      inbox: false, // chat bot in inbox | u can set true or false
+      inbox: true, // chat bot in inbox | u can set true or false
     },
     reply: {
-      sticker: false, // Boolean | ===== It not created now ======
-      audio: false, // Boolean | ===== It not created now ======
+      sticker: true, // Boolean | ===== It not created now ======
+      audio: true, // Boolean | ===== It not created now ======
     },
     presence: {
-      is: false, // U Can on or off this () | u can set true or false
+      is: true, // U Can on or off this () | u can set true or false
       value: "typing", // It has two types | u can set 'recoding' or 'typing'
     },
     read: false, // Boolean | ===== It not created now ======
